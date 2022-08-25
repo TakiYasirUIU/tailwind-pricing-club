abf3556e0db0ddacaee5c113dafd7c93945512ca
TakiYasirUIU <yasirtaki1995@gmail.com>
2022-08-26 05:08:52 +0600

    tailwind start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
+      <Navbar></Navbar>
       <h1 className='text-6xl'>Welcome to my pricing club</h1>
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2 className='text-4xl'>Save most of your money</h2>
+            <p>By giving your money ot me!!!</p>
+            
+        </div>
+    );
+};
+
+export default Header;
